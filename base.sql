02f54911945a73e4ebaab0a82b1ad8c43dd79fe9
EstebanDardillac <esteban.dardillac@gmail.com>
2023-01-17 10:50:08 +0100

    Nouvelle bdd

--- base.sql
+++ base.sql
@@ -1,35 +1,169 @@
--- Adminer 4.8.1 MySQL 5.5.5-10.4.21-MariaDB-log dump
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : mar. 17 jan. 2023 à 09:44
+-- Version du serveur :  5.7.34
+-- Version de PHP : 7.4.21
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `contact_forms`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `ProjetBank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bankaccounts`
+--
+
+CREATE TABLE `bankaccounts` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(11) NOT NULL,
+  `prenom` varchar(11) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `age` int(11) NOT NULL,
+  `adresse` varchar(11) NOT NULL,
+  `mail` varchar(11) NOT NULL,
+  `numero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact_forms`
+--
+
 CREATE TABLE `contact_forms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `fullname` varchar(100) NOT NULL,
   `phone` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
   `message` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `contact_forms` (`id`, `fullname`, `phone`, `email`, `message`, `created_at`) VALUES
-(1,	'Test 1',	'09090909',	'test1@test.com',	'Votre message',	'2023-01-16 10:50:29'),
-(2,	'Test 2',	'08080808',	'test2@test.com',	'Votre message 2',	'2023-01-16 11:03:23');
+--
+-- Déchargement des données de la table `contact_forms`
+--
 
-DROP TABLE IF EXISTS `users`;
+INSERT INTO `contact_forms` (`id`, `fullname`, `phone`, `email`, `message`, `created_at`) VALUES
+(1, 'Test 1', '09090909', 'test1@test.com', 'Votre message', '2023-01-16 10:50:29'),
+(2, 'Test 2', '08080808', 'test2@test.com', 'Votre message 2', '2023-01-16 11:03:23');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `currencies`
+--
+
+CREATE TABLE `currencies` (
+  `euro` int(11) NOT NULL,
+  `dollar` int(11) NOT NULL,
+  `livres` int(11) NOT NULL,
+  `bitcoin` int(11) NOT NULL,
+  `yens` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `deposit`
+--
+
+CREATE TABLE `deposit` (
+  `id_depositeur` int(11) NOT NULL,
+  `montant` int(250) NOT NULL,
+  `nom_monnaie` varchar(11) NOT NULL,
+  `date_et_heure_depot` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id_expediteur` int(11) NOT NULL,
+  `id_destinataire` int(11) NOT NULL,
+  `montant` int(250) NOT NULL,
+  `monnaie` varchar(11) NOT NULL,
+  `date_et_heure_trans` datetime NOT NULL,
+  `objets_transaction` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `last_ip` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_ip` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
--- 2023-01-16 13:02:57
+--
+-- Structure de la table `withdrawals`
+--
+
+CREATE TABLE `withdrawals` (
+  `id_retraiteur` int(11) NOT NULL,
+  `montant` int(250) NOT NULL,
+  `nom_monnaie` varchar(11) NOT NULL,
+  `date_et_heure_retrait` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `contact_forms`
+--
+ALTER TABLE `contact_forms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `contact_forms`
+--
+ALTER TABLE `contact_forms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
