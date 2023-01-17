043ce2c368a743ab5ca67d571363a980b99697ba
OnyxXs <timeo.donnaes@gmail.com>
2023-01-17 16:43:12 +0100

    update style.css and www/index.php

--- www/index.php
+++ www/index.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../src/templates/partials/html_head.php';
 <body>
 
 <div>
-    <h1 class="title">The Bank</h1>
+    <h1 class="title , contour">The Bank</h1>
 </div>
 
 <?php require_once __DIR__ . '/../src/templates/partials/footer.php'; ?>
