0a33c356bf3027cc96ed5bb4ef0c80a52ab774fe
OnyxXs <timeo.donnaes@gmail.com>
2023-01-18 09:35:31 +0100

    update html_end.php, style.css and www/index.php

--- www/index.php
+++ www/index.php
@@ -10,9 +10,17 @@ require_once __DIR__ . '/../src/templates/partials/html_head.php';
 ?>
 <body>
 
-<div>
-    <h1 class="title , contour">The Bank</h1>
-</div>
+<header>
+    <h1>The Bank</h1>
+</header>
+
+<section class="sect1">
+
+    <div>
+    <img src="https://dk85klopn8z0j.cloudfront.net/wp-content/uploads/2012/09/Choisir-une-banque.jpg" width="500" height="375">
+    </div>
+
+</section>
 
 <?php require_once __DIR__ . '/../src/templates/partials/footer.php'; ?>
 </body>
