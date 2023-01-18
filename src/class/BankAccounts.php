7ae9a8a79e7a7e33580e10b5cb29284a088de9e3
EstebanDardillac <esteban.dardillac@gmail.com>
2023-01-18 09:13:26 +0100

    Double Yo

---
+++ src/class/BankAccounts.php
@@ -0,0 +1,15 @@
+<?php
+
+class BankAccounts extends DbObject {
+    public $id;
+	public $nom;
+	public $prenom;
+	public $dateNaissance;
+	public $age;
+	public $adresse;
+    public $mail;
+    public $numero
+
+}
+
+?>
