d102ce2ed9da74096d2d4d9744f1efe67d3a37fc
Henry <benjamin.henry@edu.esiee-it.fr>
2023-01-18 15:40:39 +0100

    css login

--- www/login.php
+++ www/login.php
@@ -4,25 +4,28 @@ require_once __DIR__ . '/../src/init.php';
 // $db
 // $_SESSION
 
-$page_title = 'Home page';
+$page_title = 'Login';
 require_once __DIR__ . '/../src/templates/partials/html_head.php';
 
 ?>
 <body>
+<link rel="stylesheet" href="/www/assets/login.css">
 
-<h1>Login</h1>
+<h1 class="titre">Login</h1>
 
+<div id="contenu">
 <form action="/actions/login.php" method="post">
-	<div>
+	<div id="email">
 		<label for="email">Email</label>
 		<input type="text" id="email" name="email">
 	</div>
-	<div>
+	<div id="password">
 		<label for="password">Mot de passe</label>
 		<input type="password" id="password" name="password">
 	</div>
-	<button type="submit">Login</button>
+	<button id="button" type="submit">Login</button>
 </form>
+</div>
 
 <?php require_once __DIR__ . '/../src/templates/partials/footer.php'; ?>
 </body>
