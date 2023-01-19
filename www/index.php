8745d3587a08d809d3a32c99caddeb43839149f2
OnyxXs <timeo.donnaes@gmail.com>
2023-01-19 11:18:00 +0100

    update style.css and www/index.php

--- www/index.php
+++ www/index.php
@@ -27,7 +27,7 @@ require_once __DIR__ . '/../src/templates/partials/html_head.php';
 
     <img src="https://dk85klopn8z0j.cloudfront.net/wp-content/uploads/2012/09/Choisir-une-banque.jpg" width="500" height="375">
     
-    <p id="Lorems1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eveniet eaque tempore fuga quisquam cumque, voluptates voluptas quis labore id neque, iure consectetur iste tempora eos quas ducimus minus possimus unde nemo! Nesciunt unde magnam sint quibusdam temporibus nobis dolor corporis laborum et cumque? At hic dolore culpa impedit soluta?</p>
+    <p id="text1">ResponsiveBank est une banque en ligne innovante qui se concentre sur la satisfaction de ses clients. Avec une interface utilisateur conviviale et des outils de gestion de comptes en ligne avancés, ResponsiveBank facilite la gestion de vos finances quotidiennes. La sécurité est une priorité absolue pour ResponsiveBank, avec des protocoles de sécurité de pointe pour protéger vos informations personnelles et financières. En outre, ResponsiveBank offre des taux d'intérêt compétitifs sur ses comptes d'épargne et des options de crédit flexibles pour ses clients qualifiés. Avec ResponsiveBank, vous pouvez être sûr que vos finances sont entre de bonnes mains.</p>
 
 </section>
 
