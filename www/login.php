4c510eeb3e165aeb6f11fc7a0b97c39b143991e2
Henry <benjamin.henry@edu.esiee-it.fr>
2023-01-20 10:20:49 +0100

    t

--- www/login.php
+++ www/login.php
@@ -1,5 +1,5 @@
 <?php 
-                require_once __DIR__ . '../../src/init.php'
+                require_once __DIR__ . '../../src/init.php';
                 if(isset($_GET['login_err']))
                 {
                     $err = htmlspecialchars($_GET['login_err']);
