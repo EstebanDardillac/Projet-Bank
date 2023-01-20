d152f0d9eb78c678bab4cba8f46e6e9aa3ac595f
EstebanDardillac <esteban.dardillac@gmail.com>
2023-01-20 12:22:50 +0100

    nouveau truc

--- www/signup_traitement.php
+++ www/signup_traitement.php
@@ -11,7 +11,7 @@
         $prenom = htmlspecialchars($_POST['prenom']);
         $email = htmlspecialchars($_POST['email']);
         $mdp = hash('sha256', $_POST['mdp']);
-        $mdp_retype = htmlspecialchars($_POST['mdp_retype']);
+        $mdp_retype = hash('sha256', $_POST['mdp_retype']);
         $client_number = rand();
         // On vérifie si l'users existe
         $check = $db->prepare('SELECT email FROM users WHERE email = ?'); 
