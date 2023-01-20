e3dbe2f41669ba737fb2eb25dc3965e9d35c53b6
OnyxXs <timeo.donnaes@gmail.com>
2023-01-20 13:05:37 +0100

    update signup.php

--- src/templates/partials/html_head.php
+++ src/templates/partials/html_head.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="NoS1gnal"/>
     <title><?= $page_title; ?></title>
     <link rel="stylesheet" href="/www/assets/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
     <link rel="icon" type="image/x-icon" href="https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png">
 </head>
